c0ab010ad42c8f6d865bec671facc0290794cd4b
김수현 <tngus090909@naver.com>
2025-06-04 23:07:43 +0900

    Fix merge conflicts in MemberRepository.java and pom.xml

--- cook/src/main/java/com/dita/persistence/MemberRepository.java
+++ cook/src/main/java/com/dita/persistence/MemberRepository.java
@@ -8,7 +8,13 @@ import com.dita.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, String> { // 기본키가 Long이라 가정
 
+<<<<<<< Updated upstream
     Optional<Member> findByEmail(String email);
 
     boolean existsByUserId(String userId);
+=======
+public interface MemberRepository extends JpaRepository<Member, String> {
+  Optional<Member> findByUserId(String userId);
+ 
+>>>>>>> Stashed changes
 }
